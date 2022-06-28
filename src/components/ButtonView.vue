368ad6bfc0de445699b15741b9b134f859ee5dcd
yrkim <icho0405@gmail.com>
2022-06-28 17:29:41 +0900

    add: buttonView component

---
+++ src/components/ButtonView.vue
@@ -0,0 +1,24 @@
+<template>
+  <button type="버튼" @click="onClick">버튼</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonView',
+  methods: {
+    onClick() {
+      console.log('===== onClick');
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  width: 100px;
+  height: 50px;
+  background: var(--primary);
+  color: var(--white);
+  border-radius: 10px;
+}
+</style>
--- src/views/PointView.vue
+++ src/views/PointView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="point">
     <h1>ATNP 포인트</h1>
+    <h2>Hello! {{ userName }} 👋</h2>
+    <button-view /><button-view />
     <select-view />
     <input-view />
     <check-box-view />
@@ -11,10 +13,11 @@
 import InputView from '@/components/InputView.vue';
 import SelectView from '@/components/SelectView.vue';
 import CheckBoxView from '@/components/CheckBoxView.vue';
+import ButtonView from '@/components/ButtonView.vue';
 
 export default {
   name: 'PointView',
-  components: { InputView, SelectView, CheckBoxView },
+  components: { InputView, SelectView, CheckBoxView, ButtonView },
   data() {
     return {
       userName: 'April',
@@ -23,10 +26,16 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 h1 {
   padding: 30px;
   font-size: 36px;
+  font-weight: 600;
+}
+
+h2 {
+  padding: 30px;
+  font-size: 28px;
   font-weight: 600;
 }
 </style>
