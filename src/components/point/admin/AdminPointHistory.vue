481e3a20dad914fc52edfe49b3f674cbbaa11b41
yrkim <icho0405@gmail.com>
2022-07-10 10:15:25 +0900

    modify: pointHistory

--- src/components/point/admin/AdminPointHistory.vue
+++ src/components/point/admin/AdminPointHistory.vue
@@ -69,6 +69,22 @@
           </span>
           <span class="label">{{ (4000 / 4) * 3 }}</span>
         </li>
+        <li class="value">
+          <span class="label">04.07</span>
+          <span class="labelLayout">
+            <span class="label">식대초과</span>
+            <span class="label">도마</span>
+          </span>
+          <span class="labelLayout">
+            <span class="label">프릴, 제임스, 찰스</span>
+            <span class="label">하울</span>
+          </span>
+          <span class="labelLayout">
+            <span class="label">4000</span>
+            <span class="label">{{ 4000 / 4 }}</span>
+          </span>
+          <span class="label">{{ (4000 / 4) * 3 }}</span>
+        </li>
       </ul>
     </section>
   </accordion-view>
