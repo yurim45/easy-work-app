e91d163591e5ebe18cbbde54ca596c55d0b22659
yrkim <icho0405@gmail.com>
2022-07-11 16:33:01 +0900

    modify: data

--- src/components/point/admin/AdminPointHistory.vue
+++ src/components/point/admin/AdminPointHistory.vue
@@ -38,22 +38,6 @@
           <span class="label">차감사용</span>
         </li>
         <li class="value">
-          <span class="label">04.05</span>
-          <span class="labelLayout">
-            <span class="label">식대초과</span>
-            <span class="label">도마</span>
-          </span>
-          <span class="labelLayout">
-            <span class="label">프릴, 제임스, 찰스</span>
-            <span class="label">하울</span>
-          </span>
-          <span class="labelLayout">
-            <span class="label">4000</span>
-            <span class="label">{{ 4000 / 4 }}</span>
-          </span>
-          <span class="label">{{ (4000 / 4) * 3 }}</span>
-        </li>
-        <li class="value">
           <span class="label">04.07</span>
           <span class="labelLayout">
             <span class="label">식대초과</span>
